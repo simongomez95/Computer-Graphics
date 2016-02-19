15281645a8ae6cd8cc0d4774f1a18d6aa63bff10
simon gomez <nekothecat@MacBook-de-simon.local>
2016-02-19 16:12:52 -0500

    Matrix2d

--- Reto3/src/edu/reto3/Matriz2D.java
+++ Reto3/src/edu/reto3/Matriz2D.java
@@ -7,12 +7,41 @@ public class Matriz2D {
 
     private double[][] matriz;
 
-    public Matriz2D sumarMatriz(Matriz2D otro) {
+    public Matriz2D(){}
+
+    public double[][] getMatriz() {
+
+        return matriz;
 
     }
 
-    public Matriz2D sumarEscalar(double otro) {
+    public Matriz2D sumarMatriz(Matriz2D otro) {
 
+
+        Matriz2D resultado;
+
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                matriz[i][j] = matriz[i][j] + otro.getMatriz()[i][j];
+
+            }
+        }
+
+        return resultado;
+    }
+
+    public Matriz2D multiplicarEscalar(double otro) {
+
+        Matriz2D resultado;
+
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                matriz[i][j] = matriz[i][j]*otro;
+            }
+        }
+
+
+        return resultado;
     }
 
 }
